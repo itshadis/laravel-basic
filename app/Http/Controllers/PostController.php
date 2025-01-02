c6e88ecbcdff4c631b3e4e5195ebed3209c059c1
itshadis <hadis1098@gmail.com>
2025-01-02 21:25:20 +0700

    add column by migrate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = DB::table('posts')->get();
+        $posts = DB::table('posts')->where('active', true)->get();
         $data = [
             'posts' => $posts
         ];
